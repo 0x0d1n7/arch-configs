d1de21ee1cf95a230e6763301b3f92678e7b1d2e
diny <34692250+0x0d1n7@users.noreply.github.com>
2023-05-30 12:41:27 +0300

    Update apps.sh

--- apps.sh
+++ apps.sh
@@ -1,7 +1,59 @@
 #!/bin/bash
 
 # Array to store program names
-declare -a programs=("gobuster" "shellter" "fluxion" "airgeddon" "beef" "burpsuite" "dhcpig" "chntpw" "whatweb" "macchanger" "netcat" "bettercap" "recon-ng" "netdiscover" "ffuf" "wifiphisher" "sqlmap" "metasploit" "wireshark" "shellter" "evillimiter" "netTool" "httrack" "speedtest" "speedtest-cli" "mariadb" "mysql" "macchanger" "mbpfan" "hollywood" "cowsay")
+declare -a programs=(
+  "gobuster" 
+  "shellter" 
+  "fluxion" 
+  "airgeddon" 
+  "beef" 
+  "burpsuite" 
+  "dhcpig" 
+  "chntpw" 
+  "whatweb" 
+  "macchanger" 
+  "netcat" 
+  "bettercap" 
+  "recon-ng" 
+  "netdiscover" 
+  "ffuf" 
+  "wifiphisher" 
+  "sqlmap" 
+  "metasploit" 
+  "wireshark" 
+  "shellter" 
+  "evillimiter" 
+  "netTool" 
+  "httrack" 
+  "speedtest" 
+  "speedtest-cli" 
+  "mariadb" 
+  "mysql" 
+  "macchanger" 
+  "mbpfan" 
+  "hollywood" 
+  "cowsay" 
+  "nmap" 
+  "openvas" 
+  "nikto" 
+  "dirb" 
+  "enum4linux" 
+  "john" 
+  "hydra" 
+  "hashcat" 
+  "sqlninja" 
+  "yersinia" 
+  "arp-scan" 
+  "dnsenum" 
+  "fimap" 
+  "wpscan" 
+  "mitmproxy" 
+  "snort" 
+  "lynis"
+)
+
+# Array to store programs that are not available
+declare -a not_available=()
 
 # Function to install programs using Pacman or Yay
 install_programs() {
@@ -11,8 +63,8 @@ install_programs() {
       echo "Installing $program using Pacman..."
       sudo pacman -S --noconfirm "$program"
     else
-      echo "Program $program not found in Pacman repositories."
-      # You can add custom logic here to handle installation from other sources or skipping installation
+      echo "Program $program not found in Pacman repositories. Skipping installation."
+      not_available+=("$program")
     fi
   done
 }
@@ -22,3 +74,10 @@ programs=($(echo "${programs[@]}" | tr ' ' '\n' | sort -u | tr '\n' ' '))
 
 # Install programs
 install_programs
+
+# Save the list of programs that are not available to a text file
+if [ ${#not_available[@]} -gt 0 ]; then
+  printf '%s\n' "${not_available[@]}" > not_available.txt
+  echo "The following programs were not found in the Pacman repositories and were saved to not_available.txt:"
+  cat not_available.txt
+fi
